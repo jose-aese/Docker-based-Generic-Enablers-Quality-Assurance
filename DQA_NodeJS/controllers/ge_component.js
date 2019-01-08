606bdca39e2a7b76aed52d44a27346dbaa589fc1
gusbg <38957081+gusbg@users.noreply.github.com>
2019-01-08 17:25:31 -0600

    Update ge_component.js

--- DQA_NodeJS/controllers/ge_component.js
+++ DQA_NodeJS/controllers/ge_component.js
@@ -1,43 +1,58 @@
 'use stric'
-
+//Importtar modelo ge_component.js.
 var ge_component = require('../models/ge_component');
+//Importar libreria File Server para trabajar con el sistema de archivos del servidor.
 var fs = require('fs');
+//Importar libreria path para manipular las rutas de los ficheros.
 var path = require('path');
-
 // Registro de nuevo ge_component
+//Función utilizada para registrar un nuevo habilitador en la base de datos.
 function saveGeComponent(req, res){
+    //Extraer datos contenidos en el objeto JSON de la petición entrante.
     var params = req.body;
+    //Instanciando modelo ge_component.js
     var ge_comp = new ge_component();
     
-    
-
-    if(params.ge && params.url){
+    //Comprovar que todos los parametros necesarios hayan sido enviados por el usurio.
+    if(params.ge && params.url){ 
+        //Capturar nombre de GE y convertir cadena entrante en minusculas.
         ge_comp.ge = params.ge.toLowerCase();
+        //Capturar parametro URL. 
         ge_comp.url = params.url;
+        //Igualar atributo image al valor null
         ge_comp.image = null;
         
         // control de ge_component duplicado
         ge_component.find(
+            //Consulta a la colección (ges) de la base de datos (ge_docker) para verificar que no exista el nuevo GE 
             {ge: ge_comp.ge.toLowerCase()}
+            //El resultado de la consulta se guarda en la varible (ges_comp).
         ).exec((err, ges_comp) => {
+            //Si ocurre algun error durante la petición se retorna.
             if(err) 
                 return res.status(500).send({message:'Error en la petición'});
-
-            if(ges_comp && ges_comp.length >= 1){
+            //Verificar si existe algún dato en la varible (ges_comp).
+            if(ges_comp && ges_comp.length >= 1){ 
+                //Si existe dato en (ges_comp) signifiaca que ya existe el GE, se retornara un mensaje al usuario
                 return res.status(500).send({message:'El GE component ya existe'});
             }else{
-                ge_comp.save((err, ge_compStored) => {
+                //Función para guardar objeto (ge_comp) en la colección (ges). 
+                ge_comp.save((err, ge_compStored) => {//Retorna un (err) o un objeto (ge_compStored).
+                    //Si ocurre algún error durante el proceso de guardar el objeto se retornara el error obtenido. 
                     if(err) return res.status(500).send({message:'error al guardar GE component'});
-                        
-                    if(ge_compStored){
+                    //Si se guardo correctamente el objeto (ge_comp) en la colección la base retornara un objeto JSON
+                    //y se guardara en la varible (ge_compStored).
+                    if(ge_compStored){//Comprobar que exista varible (ge_compStored)
+                        //Retornar objeto (ge_compStored) como confirmación de transacción realizada con exito.
                         res.status(200).send({ge_comp:ge_compStored});
                     }else{
+                        //Retornar mensaje de transaccción no realizada con exito.
                         res.status(404).send({message:'No se ha registrado el GE component'});
                     }
                 });
             }
         });
-        
+    //Retornar mensaje pidiendo al usuario que ingrese todos los parametros necesarios    
     }else{
         res.status(200).send({
             message:'Envia todos los campos necesarios!!!'
