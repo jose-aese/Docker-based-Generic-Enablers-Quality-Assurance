ee7ec7f1f184b753b7926ecd559dd5f4a523a115
jose-aese <45864492+jose-aese@users.noreply.github.com>
2019-01-15 11:20:58 -0600

    Update index.py

--- index.py
+++ index.py
@@ -1,9 +1,9 @@
 import os 
-
+import time
 print('------------- install MongoDB -------------')
 os.system('docker rm -f fiware_report && docker rm -f fiware_selenium && docker rm -f fiware_chrome')#Eliminar contenedores
 os.system('cd DQA_Compose && docker-compose up -d')#Desplegando contendores
-
+time.sleep(5)
 print('------------- Start DockerQA -------------')
 print('----To stop the service type ctr+c------')
 os.system('cd DQA_NodeJS && node index.js')#Levantando servicio
