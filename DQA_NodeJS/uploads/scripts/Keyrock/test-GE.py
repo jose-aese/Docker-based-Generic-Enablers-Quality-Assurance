4f285bfe5f70be550ac3081647621bdbaee6ba0b
jose-aese <45864492+jose-aese@users.noreply.github.com>
2019-01-15 16:36:21 -0600

    Update test-GE.py

--- DQA_NodeJS/uploads/scripts/Keyrock/test-GE.py
+++ DQA_NodeJS/uploads/scripts/Keyrock/test-GE.py
@@ -14,14 +14,33 @@ class idm(unittest.TestCase):
 			os.system('git clone https://github.com/ging/fiware-idm.git')
 		version = subprocess.check_output("cd ./fiware-idm && git describe --tags", shell=True).strip()
 		os.system('cd ./fiware-idm/extras/docker && docker-compose up -d')
-		sleep(15)
+		sleep(5)
 		fecha = strftime("%d/%m/%y")
 		hora = strftime("%H:%M:%S")
 		host_name = os.popen("hostname").readline()
-		sleep(20)
-		resultado = os.popen("cd ./fiware-idm/extras/docker && docker-compose ps").readlines()
+		sleep(5)
 		resultadoid = os.popen("docker ps").readlines()
-		contenedor =[host_name, fecha, hora, version, resultado[2].split()[0], resultadoid[1].split()[0], resultado[2].split()[3], resultado[3].split()[0], resultadoid[2].split()[0], resultado[3].split()[3]]
+		total1 = resultadoid[1].split()
+		numtotal1 = 0
+		for can in total1:
+			numtotal1 += 1
+		name = total1[numtotal1-1]
+		id = total1[0]
+		status = 'error'
+		for can in total1:
+			if can == 'Up':
+				status = 'Up'
+		total2 = resultadoid[2].split()
+		numtotal2 = 0
+		for can in total2:
+			numtotal2 += 1
+		name2 = total2[numtotal2-1]
+		id2 = total1[0]
+		status2 = 'error'
+		for can in total1:
+			if can == 'Up':
+				status2 = 'Up'
+		contenedor =[host_name, fecha, hora, version,name,id,status,name2, id2, status2 ]
 		r = open('report.txt','w')
 		r.write(str(contenedor[1]+','+contenedor[2]+','+contenedor[3]+','+contenedor[4]+','+contenedor[5]+','+contenedor[6]+','+contenedor[7]+','+contenedor[8]+','+contenedor[9]+','+contenedor[0]))
 
@@ -36,4 +55,3 @@ if __name__ == "__main__":
 	unittest.main(testRunner=runner)
 	f.close()
 
-#Samuel Castillo Texocotitla
