d693e11194580dbd6e582f9925922fce21df2ee6
jose-aese <45864492+jose-aese@users.noreply.github.com>
2019-01-16 18:29:57 -0600

    Update geoServer_test.py

--- DQA_NodeJS/uploads/scripts/Geoserver/geoServer_test.py
+++ DQA_NodeJS/uploads/scripts/Geoserver/geoServer_test.py
@@ -15,28 +15,27 @@ class geoserver(unittest.TestCase):
         else:
             os.system('git clone https://github.com/Cyberlightning/GISDataProvider') #Clonar directorio
             os.chdir('GISDataProvider')
-        
         #version = subprocess.check_output('git describe --tags', shell=True).strip() #Obtener version desde github
         version = os.popen('git tag').readlines().pop().rstrip('\n')
         date = strftime('%y-%m-%d') #Fecha de ejecucion
         hour = strftime('%H:%M:%S') #Hora de ejecucion
         host_name = os.popen('hostname').readline().rstrip('\n') #Nombre del Host de ejecucion
-
         os.system('docker run -d -it -p 9090:8080 --name geoServer fiware/gisdataprovider') #Creacion del contenedor
-        
         info = os.popen('docker ps -f "name=geoServer"') #Extraer la informacion del estado del contenedor
         container_info = info.readlines() #Gaurdar informacion en arreglo
-        containerID = container_info[1].split()[0] #Extraer el conytainer id del arreglo
-        containerStatus = container_info[1].split()[7] #Extraer el Status del arreglo
-        
+        info_split = container_info[1].split()
+        numtotal = 0
+        for can in info_split:
+            numtotal += 1
+        containerID = info_split[0] #Extraer el conytainer id del arreglo
+        containerStatus = 'error'
+        for can in info_split:
+            if can == 'Up':
+                containerStatus = 'Up'	
         time.sleep(5)
         os.system('curl -I localhost:9090/geoserver/web/') #Respuesta por parte del servidor dentro del contenedor
-
         containerName = os.popen('docker rm geoServer -f').readline().rstrip('\n') #Detener contenedor y guardar su nombre        
-
         test_info =[host_name,date,hour,version,containerStatus,containerID,containerName] #Guardar valores obtenidos del test en arreglo
-        print (test_info)    
-
         os.chdir('../')
         f = open('report.txt','w') #Abrir archivo report.txt
         f.write(test_info[1]+','+test_info[2]+','+test_info[3]+','+test_info[6]+','+test_info[4]+','+test_info[5]+','+test_info[0])#Escribir en archivo   
